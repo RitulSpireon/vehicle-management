be3dbdfdf7ec1802645922a19055eeefcf657de1
ritul.gupta <ritulgupta05@gmail.com>
2023-08-22 16:26:13 +0530

    changes

---
+++ src/main/java/com/vehiclemanagement/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.vehiclemanagement.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // You can configure specific origins here
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
